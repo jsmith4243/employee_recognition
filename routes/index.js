a091bc0955f59fe3cb3f045fca5f11d2c04a6f17
John Doe <johndoe@example.com>
2017-01-24 17:30:22 -0800

    navigation links

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/userRegistration', function(req, res, next) {
+  res.render('userRegistration', { title: 'User Registration' });
+});
+
+router.get('/administration', function(req, res, next) {
+  res.render('administration', { title: 'Administration' });
+});
+
+router.get('/awardDisplay', function(req, res, next) {
+  res.render('awardDisplay', { title: 'awardDisplay' });
+});
+
 module.exports = router;

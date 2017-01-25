2cca698fe63d405a317b6b4a1e11c60fe4e3d94e
John Doe <johndoe@example.com>
2017-01-25 01:53:16 -0800

    post parameters working

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
+/* //mongo
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+*/
+
+
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('database.db');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  console.log("xxx");
-  
+  /* //mongo
   var url = 'mongodb://localhost:27017/test';
-
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server");
     db.close();
+  
     
 
     
   });
-
+  */
   
+  
+ 
+
+  var id = req.params.id; //for get
+  
+  console.log("req.params.operation: " + req.params.id);
+  console.log("req.param('operation'): " + req.param('id'));
+  console.log("req.body.id: " + req.body.id);
+  
+  console.log("id is: " + id);
 
 
 
@@ -41,6 +61,19 @@ router.get('/administration', function(req, res, next) {
 
 router.get('/awardDisplay', function(req, res, next) {
   res.render('awardDisplay', { title: 'awardDisplay' });
+});
+
+router.post('/register', function(req, res, next) {
+
+  //db.run("INSERT INTO USERS(ID, USERNAME, PASSWORD) VALUES('2', 'user2', 'pass')");
+  
+  var id = req.body.id; //for post
+
+  
+  console.log("id is: " + id);
+  
+  res.send("User Registered");	
+
 });
 
 module.exports = router;

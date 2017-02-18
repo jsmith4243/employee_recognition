38e11fe44ef14d3e94ead7670001050767039cf7
Bryan Nguyen <bmn@berkeley.edu>
2017-02-17 19:12:33 -0800

    Add function to generate award

--- award/award.js
+++ award/award.js
@@ -23,7 +23,28 @@ var award = {
             ' Sender',
             ' }}\\end{center}\\end{document}',
         ], options);
+    },
+
+    generate: function(name, awardtype, date, sender) {
+        return latex([
+            ' \\documentclass[12pt,letter]{article}\\usepackage[landscape,margin=1in]{geometry}\\usepackage{lmodern}\\begin{document}\\begin{center}\\pagenumbering{gobble}{',
+            ' {\\fontsize{28pt}{48pt}\\selectfont',
+            ' Gemini Company',
+            ' }\\\\[24pt]{\\fontsize{36pt}{96pt}\\selectfont',
+            ' Award Certificate',
+            ' }\\\\[24pt]{\\fontsize{24pt}{5cm}\\selectfont\\emph{awarded to}}\\\\[24pt]{\\fontsize{48pt}{5cm}\\selectfont',
+            ' ' + name,
+            ' }\\\\[24pt]{\\fontsize{24pt}{5cm}\\selectfont\\emph{for}}\\\\[24pt]{\\fontsize{36pt}{5cm}\\selectfont',
+            ' ' + awardtype,
+            ' }\\\\[24pt]{\\fontsize{24pt}{5cm}\\selectfont',
+            ' ' + date,
+            ' }\\\\[24pt]{\\fontsize{24pt}{5cm}\\selectfont\\emph{by}}\\\\[24pt]{\\fontsize{24pt}{5cm}\\selectfont',
+            ' ' + sender,
+            ' }}\\end{center}\\end{document}',
+        ], options);
     }
+
+
 
 };
 module.exports = award;

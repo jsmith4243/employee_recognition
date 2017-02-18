73fb1f2f5f65a31c65f9b4dbc435f3c1f711071e
Bryan Nguyen <bmn@berkeley.edu>
2017-02-17 19:47:33 -0800

    Format date in award

--- award/award.js
+++ award/award.js
@@ -1,4 +1,5 @@
 var latex = require("latex");
+var moment = require('moment');
 
 var command = "C:\\Program \Files\\MiKTeX 2.9\\miktex\\bin\\x64\\miktex-pdflatex.exe";
 
@@ -37,7 +38,7 @@ var award = {
             ' }\\\\[24pt]{\\fontsize{24pt}{5cm}\\selectfont\\emph{for}}\\\\[24pt]{\\fontsize{36pt}{5cm}\\selectfont',
             ' ' + awardtype,
             ' }\\\\[24pt]{\\fontsize{24pt}{5cm}\\selectfont',
-            ' ' + date,
+            ' ' + moment(date, "X").format("MMMM D, YYYY"),
             ' }\\\\[24pt]{\\fontsize{24pt}{5cm}\\selectfont\\emph{by}}\\\\[24pt]{\\fontsize{24pt}{5cm}\\selectfont',
             ' ' + sender,
             ' }}\\end{center}\\end{document}',

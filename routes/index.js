a376d1d8b94b2ea91a9b04ee6513a66396870bfb
jsmith4243 <rcianpandacian@gmail.com>
2017-03-14 14:23:48 -0700

    route for chart

--- routes/index.js
+++ routes/index.js
@@ -175,6 +175,91 @@ router.post('/getuserawardcount', function(req, res, next) {
 
 });
 
+
+
+
+router.post('/getemployeeawardcount', function(req, res, next) {
+
+
+  console.log("/getemployeeawardcount received");
+
+  var resp = new Array();
+  
+  var i = 0;
+  
+  
+
+  /*    //extra field awardcount in users table implementation
+  db.all("SELECT id, username, awardcount FROM users", function(err, rows) {
+    rows.forEach(function(row) {
+      //console.log(row.id, row.username);
+      resp[i] = new Object();
+      resp[i].id = row.id;
+      resp[i].username = row.username;
+      resp[i].awardcount = row.awardcount;
+      
+      
+      i = i + 1;
+      
+      
+    })
+    
+    
+    console.log("i: " + i);
+    console.log("resp[0].username:" + resp[1].username);
+    console.log(JSON.stringify(resp));
+    
+
+
+
+    //db.all('SELECT count(*) FROM entries WHERE user =  ', function(err, users) {
+
+
+  });
+
+  */
+
+
+  db.all("SELECT user, recipient, email, count(email) AS count FROM entries LEFT OUTER JOIN users ON entries.user = users.id  GROUP BY recipient", function(err, rows) {
+    rows.forEach(function(row) {
+      //console.log(row.id, row.username);
+      resp[i] = new Object();
+      resp[i].id = row.user;
+      resp[i].username = row.recipient;
+      resp[i].awardcount = row.count;
+      
+      
+      i = i + 1;
+      
+      
+    })
+
+
+  
+
+
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(resp));
+
+  });
+
+  
+
+
+
+ 
+
+
+
+
+});
+
+
+
+
+
+
+
 router.post('/login', passport.authenticate('user-local', { successRedirect: '/' }));
 
 router.post('/admin-login', passport.authenticate('admin-local', { successRedirect: '/administration' }));

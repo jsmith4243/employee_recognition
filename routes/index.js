b9024936b2b16651dd92e18ce8b7e5b2d34afdc1
Bryan Nguyen <bmn@berkeley.edu>
2017-03-15 22:49:07 -0700

    Clean up and organize index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var crypto = require('crypto');
-var csv = require('../csv/index');
-var fs = require('fs');
 var multer = require('multer');
 var mime = require('mime');
 
@@ -19,25 +17,7 @@ var storage = multer.diskStorage({
 
 var upload = multer({ dest: 'uploads/', storage: storage});
 
-/* //mongo
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-*/
-
-var nodemailer = require('nodemailer');
-
-
-
 var passport = require('passport');
-var sqlite3 = require('sqlite3').verbose();
-var db = require('../db/index');
-
-function hashPassword(password, salt) {
-  var hash = crypto.createHash('sha256');
-  hash.update(password);
-  hash.update(salt);
-  return hash.digest('hex');
-}
 
 var award = require('./award');
 var site = require('./site');
@@ -46,54 +26,33 @@ var admin = require('./admin');
 var chart = require('./chart');
 
 router.get('/', site.index);
-
 router.get('/userRegistration', site.registration);
-
 router.get('/administration', site.administration);
-
-router.get('/chart', chart.chart);
-
-router.post('/getuserawardcount', chart.getuserawardcount);
-
-router.post('/getemployeeawardcount', chart.getemployeeawardcount);
+router.get('/userSettings', site.userSettings);
 
 router.post('/login', passport.authenticate('user-local', { successRedirect: '/' }));
-
 router.post('/admin-login', passport.authenticate('admin-local', { successRedirect: '/administration' }));
 
 router.get('/logout', user.logout);
-
 router.post('/register', upload.single('signature'), user.register);
-
 router.get('/mysignature', user.mysignature);
-
-router.get('/reports', admin.reports);
-
-router.post('/deleteuser', admin.deleteuser);
-
-router.post('/edituser', admin.edituser);
-
-router.post('/retrieveuserlist', admin.retrieveuserlist);
-
-router.post('/sendaward', award.send);
-
-router.get('/test-download', award.test);
-
-router.get('/award-preview', award.preview);
-
-router.post('/addaward', admin.addaward);
-
-router.post('/deleteaward', admin.deleteaward);
-
-router.post('/retrieveawardlist', admin.retrieveawardlist);
-
-router.get('/userSettings', site.userSettings);
-
 router.post('/edituserFromSettings', user.edituserFromSettings);
-
 router.post('/retrieveuserlistFromSettings', user.retrieveuserlistFromSettings);
 
+router.get('/test-download', award.test);
+router.get('/award-preview', award.preview);
+router.post('/sendaward', award.send);
 
+router.get('/reports', admin.reports);
+router.post('/deleteuser', admin.deleteuser);
+router.post('/edituser', admin.edituser);
+router.post('/retrieveuserlist', admin.retrieveuserlist);
+router.post('/addaward', admin.addaward);
+router.post('/deleteaward', admin.deleteaward);
+router.post('/retrieveawardlist', admin.retrieveawardlist);
 
+router.get('/chart', chart.chart);
+router.post('/getuserawardcount', chart.getuserawardcount);
+router.post('/getemployeeawardcount', chart.getemployeeawardcount);
 
 module.exports = router;
